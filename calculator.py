88aa9c4c7121396da92d8c4d0c4410ac509493d9
AviralSharma11 <aviral0201sharma@gmail.com>
2023-09-07 21:55:21 +0530

    Calculator

--- calculator.py
+++ calculator.py
@@ -17,19 +17,29 @@ elif a == 1:
             return (num * fact(num-1))
     
     x = float(input("What is x? "))
+    x %= 360
     x = x * 3.141592653589793238462643383279502884197 / 180
     sinx = 0
     cosx = 0
-    for i in range (1,86):
+    for i in range (1, 86):
         sign = pow(-1 , i+1)
-        sinx += sign * (pow(x,(2*i-1))) / fact(2*i-1)
-        cosx += sign*(pow(x,(2*i-2)))/fact(2*i-2)
-    print(f"sin of given x is",{sinx})
-    print(f"cos of given x is",{cosx})
-    print(f"tan of given x is",{sinx/cosx})
-    print(f"cosec of given x is",{1/sinx})
-    print(f"sec of given x is",{1/cosx})
-    print(f"cot of given x is",{cosx/sinx})
+        sinx += sign *(((pow(x,(2*i-1))) / fact(2*i-1)))
+        cosx += sign*((pow(x,(2*i-2)))/fact(2*i-2))
+        i += 2
+        
+    out_sinx = "{:.3f}".format(sinx)
+    out_cosx = "{:.3f}".format(cosx)
+    out_tanx = "{:.3f}".format(sinx/cosx)
+    out_cosecx = "{:.3f}".format(1/sinx)
+    out_secx = "{:.3f}".format(1/cosx)
+    out_cotx = "{:.3f}".format(cosx/sinx)
+    print(f"sin of given x is",{out_sinx})
+    print(f"cos of given x is",{out_cosx})
+    print(f"tan of given x is",{out_tanx})
+    print(f"cosec of given x is",{out_cosecx})
+    print(f"sec of given x is",{out_secx})
+    print(f"cot of given x is",{out_cotx})
+
 
     
 
